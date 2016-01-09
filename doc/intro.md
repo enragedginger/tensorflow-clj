a5c7369c1f2aaf79ceae14e15c8846e90d00f436
Stephen M. Hopper <enragedginger@gmail.com>
2016-01-09 16:04:16 -0600

    Added initial project template.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tensorflow-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tensorflow_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns tensorflow-clj.core-test
+  (:require [clojure.test :refer :all]
+            [tensorflow-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
