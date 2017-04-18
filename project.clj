9348a5af142ce3892108f60bf92ed8866ae7d784
Lassi Kortela <lassi@lassi.io>
2017-04-18 21:08:12 +0300

    Upgrade TensorFlow to 1.1.0-rc2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.tensorflow/tensorflow "1.1.0-rc1"]]
+                 [org.tensorflow/tensorflow "1.1.0-rc2"]]
   :main ^:skip-aot tensorflow-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
