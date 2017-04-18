a71b6b620558603965bccf51e2978e404c1fe302
Lassi Kortela <lassi@lassi.io>
2017-04-18 18:37:08 +0300

    Test numDimensions in addition to shape

--- test/tensorflow_clj/core_test.clj
+++ test/tensorflow_clj/core_test.clj
@@ -6,10 +6,12 @@
   (testing "Scalar tensor"
     (let [t (org.tensorflow.Tensor/create 123.0)]
       (is (= org.tensorflow.DataType/DOUBLE (.dataType t)))
+      (is (= 0 (.numDimensions t)))
       (is (= [] (vec (.shape t)))))))
 
 (deftest vector-tensor
   (testing "Vector tensor"
     (let [t (org.tensorflow.Tensor/create (into-array [1.0 2.0 3.0]))]
       (is (= org.tensorflow.DataType/DOUBLE (.dataType t)))
+      (is (= 1 (.numDimensions t)))
       (is (= [3] (vec (.shape t)))))))
