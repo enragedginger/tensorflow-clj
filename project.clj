ec8cc830dd9bf9e7284a754a465ef126ee9d70a9
Lassi Kortela <lassi@lassi.io>
2017-04-22 02:38:39 +0300

    Add core.matrix

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/core.matrix "0.58.0"]
                  [org.tensorflow/tensorflow "1.1.0-rc2"]]
   :main ^:skip-aot tensorflow-clj.core
   :target-path "target/%s"
