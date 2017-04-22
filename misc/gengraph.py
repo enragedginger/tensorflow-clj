5965a03502cf4b1d697a00a0d4c6aff2dc66b3db
Lassi Kortela <lassi@lassi.io>
2017-04-22 12:24:13 +0300

    Add linreg graph
    It's the linear regression model from
    https://www.tensorflow.org/get_started/get_started

--- misc/gengraph.py
+++ misc/gengraph.py
@@ -28,3 +28,20 @@ with gen("mulbymat"):
 with gen("mul2vars"):
     tf.placeholder(tf.float32, name="a") * \
         tf.placeholder(tf.float32, name="b")
+
+with gen("linreg"):
+    W = tf.Variable([.3], tf.float32, name="W")
+    b = tf.Variable([-.3], tf.float32, name="b")
+    x = tf.placeholder(tf.float32, name="x")
+    linear_model = W * x + b
+    tf.identity(linear_model, name="linear_model")
+
+    y = tf.placeholder(tf.float32, name="y")
+    squared_deltas = tf.square(linear_model - y, name="squared_deltas")
+    loss = tf.reduce_sum(squared_deltas, name="loss")
+
+    fixW = tf.assign(W, [-1.], name="fixW")
+    fixb = tf.assign(b, [1.], name="fixb")
+
+    init = tf.global_variables_initializer()
+    #tf.identity(init, name="init")
