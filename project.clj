5f0587405a21a98f67972fefbd2e69a42c70043a
Stephen Hopper <stephenmhopper@gmail.com>
2017-04-23 11:27:25 -0500

    Added signing key and pushed a build to clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject tensorflow-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tensorflow-clj "0.1"
+  :description "Gateway from Clojure to Tensorflow"
+  :url "https://github.com/enragedginger/tensorflow-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.58.0"]
                  [org.tensorflow/tensorflow "1.1.0-rc2"]]
+  :signing {:gpg-key "enragedginger@gmail.com"}
   :main ^:skip-aot tensorflow-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
