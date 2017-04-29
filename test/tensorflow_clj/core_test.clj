d3350ba215a94ecc886b2ad792e25fc32d9c4362
Lassi Kortela <lassi@lassi.io>
2017-04-30 00:06:33 +0300

    Remove existing linear regression tests
    We didn't really know what we were doing there. The tests passed, but
    they didn't test the intended TF features properly. These tests were
    mainly a placeholder until we figured out how to make TF initialize
    variables.

--- test/tensorflow_clj/core_test.clj
+++ test/tensorflow_clj/core_test.clj
@@ -70,21 +70,3 @@
                             :b [[1. 2.] [3. 4.]]}
                   :mul)]
         (is (= v [[1. -2.] [6. -8.]]))))))
-
-(deftest linreg-graph
-  (testing "Linear regression"
-    (let [x-train [1. 2. 3. 4.]
-          y-train [0. -1. -2. -3.]]
-      (with-graph-file "misc/linreg.pb"
-        (let [[v] (run-graph {:x x-train :y y-train :W [-1.] :b [1.]}
-                    :loss)]
-          (is (= v 0.0)))))
-    (let [W -1.
-          b 1.
-          x-train (into [] (map (fn [i] (* 1000 (Math/random))) (range 1000)))
-          y-train (map #(+ (* W %) b) x-train)]
-      (with-graph-file
-        "misc/linreg.pb"
-        (let [[v] (run-graph {:x x-train :y y-train :W [W] :b [b]}
-                             :loss)]
-          (is (approx= 0.0 v)))))))
