ff17d72a1b7174e5e0016b660d29b0a9f36539a0
Lassi Kortela <lassi@lassi.io>
2017-04-30 00:09:17 +0300

    Add real linear regression tests
    These should be a 1:1 Python->Clojure translation of the lin reg
    examples at https://www.tensorflow.org/get_started/get_started

--- test/tensorflow_clj/core_test.clj
+++ test/tensorflow_clj/core_test.clj
@@ -70,3 +70,40 @@
                             :b [[1. 2.] [3. 4.]]}
                   :mul)]
         (is (= v [[1. -2.] [6. -8.]]))))))
+
+(def x-train [1.  2.  3.  4.])
+(def y-train [0. -1. -2. -3.])
+
+(deftest linreg-one-pass
+  (testing "Linear regression (one pass)"
+    (with-graph-file "misc/linreg.pb"
+      (run-graph {:init nil})
+      (let [[[a b c d]] (run-graph {:x x-train} :linear_model)]
+        (is (approx= 0.0 a))
+        (is (approx= 0.3 b))
+        (is (approx= 0.6 c))
+        (is (approx= 0.9 d))))))
+
+(deftest linreg-one-loss
+  (testing "Linear regression (one loss)"
+    (with-graph-file "misc/linreg.pb"
+      (run-graph {:init nil})
+      (let [[loss] (run-graph {:x x-train :y y-train} :loss)]
+        (is (approx= 23.66 loss))))))
+
+(deftest linreg-fixed-vars
+  (testing "Linear regression (fixed variables)"
+    (with-graph-file "misc/linreg.pb"
+      (run-graph {:fixW nil :fixb nil})
+      (let [[loss] (run-graph {:x x-train :y y-train} :loss)]
+        (is (approx= 0.0 loss))))))
+
+(deftest linreg-graph-iterations
+  (testing "Linear regression (1000 iterations)"
+    (with-graph-file "misc/linreg.pb"
+      (run-graph {:init nil})
+      (dotimes [i 1000]
+        (run-graph {:x x-train :y y-train :train nil}))
+      (let [[[W] [b]] (run-graph {} :W :b)]
+        (is (approx= -0.9999 W))
+        (is (approx= 0.99999 b))))))
